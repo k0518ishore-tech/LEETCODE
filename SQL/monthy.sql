b4892b009743a873a3c6f5fa1384ea4abe1431ae
k0518ishore-tech <k0518ishore@gmail.com>
2026-07-23 22:02:24 +0530

    Add SQL query for monthly transaction summary

---
+++ SQL/monthy.sql
@@ -0,0 +1,12 @@
+1193. Monthly Transactions I
+
+# Write your MySQL query statement below
+SELECT 
+    DATE_FORMAT(trans_date, '%Y-%m') AS month, 
+    country, 
+    COUNT(id) as trans_count, 
+    SUM(state='approved') AS approved_count,
+    SUM(amount) as trans_total_amount,
+    SUM(IF(state='approved', amount, 0)) AS approved_total_amount
+FROM Transactions
+GROUP BY month, country;
