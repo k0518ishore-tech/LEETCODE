453ee7ebc525dabb9abf5ff05b3af33b7304f23d
k0518ishore-tech <k0518ishore@gmail.com>
2026-07-23 21:47:27 +0530

    Add minOperations method to Solution class
    Implement a method to calculate minimum operations to equalize elements based on queries.

---
+++ makesameelement.py
@@ -0,0 +1,13 @@
+//The method calculates the minimum operations needed to make all elements equal for given queries.
+
+class Solution:
+    def minOperations(self, nums: List[int], queries: List[int]) -> List[int]:
+        nums.sort()
+        p = [0]
+        for x in nums:
+            p.append(p[-1]+x)
+        ret = []
+        for q in queries:
+            idx = bisect.bisect_left(nums,q)
+            ret.append(idx *q - p[idx] +(p[-1]-p[idx]) - (len(p)-1 - idx) * q)
+        return ret
